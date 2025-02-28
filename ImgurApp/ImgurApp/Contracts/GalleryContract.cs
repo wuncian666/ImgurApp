6c9757f3d8110d94e51f8a09e1dfb5d6049abe88
wuncian <wuncian666@gmail.com>
2025-02-28 21:47:11 +0800

    impl mvp for gallery search

---
+++ ImgurApp/ImgurApp/Contracts/GalleryContract.cs
@@ -0,0 +1,20 @@
+﻿using ImgurAPI.Models;
+using ImgurAPI.Models.Params;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurApp.Contracts
+{
+    public interface IGalleryPresenter
+    {
+        Task SearchGalleryAsync(GallerySearchParam param);
+    }
+
+    public interface IGalleryView
+    {
+        void ShowGallery(GallerySearchModel response);
+    }
+}
---
+++ ImgurApp/ImgurApp/Presenters/GalleryPresenter.cs
@@ -0,0 +1,29 @@
+﻿using ImgurAPI.Models;
+using ImgurAPI.Models.Params;
+using ImgurApp.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurApp.Presenters
+{
+    internal class GalleryPresenter : IGalleryPresenter
+    {
+        private readonly IGalleryView _view;
+
+        public GalleryPresenter(IGalleryView view)
+        {
+            this._view = view;
+        }
+
+        public async Task SearchGalleryAsync(GallerySearchParam param)
+        {
+            ImgurAPI.ImgurContext context = new ImgurAPI.ImgurContext();
+            GallerySearchModel model =
+                await context.Gallery.GallerySearch(param);
+            this._view.ShowGallery(model);
+        }
+    }
+}
