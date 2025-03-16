fa0894c36e74ff85fafa691ace5c8f6a38aebb98
wuncian <wuncian666@gmail.com>
2025-03-16 22:13:41 +0800

    impl mvp for gallery item

---
+++ ImgurApp/ImgurApp/Contracts/GalleryItemContract.cs
@@ -0,0 +1,20 @@
+﻿using ImgurAPI.Models;
+using ImgurApp.Components.GalleryItemComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurApp.Contracts
+{
+    public interface IGalleryItemPresenter
+    {
+        Task AlbumOrImageVoting(String galleryHash, Vote vote);
+    }
+
+    public interface IGalleryItemView
+    {
+        void UpdateGalleryItem(Vote vote);
+    }
+}
---
+++ ImgurApp/ImgurApp/Presenters/GalleryItemPresenter.cs
@@ -0,0 +1,34 @@
+﻿using ImgurAPI.Models;
+using ImgurApp.Components.GalleryItemComponent;
+using ImgurApp.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurApp.Presenters
+{
+    internal class GalleryItemPresenter : IGalleryItemPresenter
+    {
+        private readonly IGalleryItemView _view;
+
+        public GalleryItemPresenter(IGalleryItemView view)
+        {
+            this._view = view;
+        }
+
+        public async Task AlbumOrImageVoting(string galleryHash, Vote vote)
+        {
+            ImgurAPI.ImgurContext context = new ImgurAPI.ImgurContext();
+            string voteStr = vote.ToString().ToLower();
+            AlbumImageVotingModel response =
+                await context.Album.AlbumImageVoting(galleryHash, voteStr);
+            if (response != null && response.success && response.status == 200)
+            {
+                Console.WriteLine("vote success");
+                this._view.UpdateGalleryItem(vote);
+            }
+        }
+    }
+}
