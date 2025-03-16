5f355694a12bdd5d92cbdf2481e47838bde4a06b
wuncian <wuncian666@gmail.com>
2025-03-16 22:15:16 +0800

    enum vote for control vote status

---
+++ ImgurApp/ImgurApp/Components/GalleryItemComponent/Vote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurApp.Components.GalleryItemComponent
+{
+    public enum Vote
+    {
+        Up,
+        Down,
+        Veto,
+    }
+}
