d581d009d09c833197a73f8bebdbf224a189c3bb
wuncian <wuncian666@gmail.com>
2025-04-11 22:39:49 +0800

    新增評論處理功能及錯誤處理機制
    在 `CommentsPresenter.cs` 中，新增對 `ImgurApp.Models` 和 `Newtonsoft.Json` 的引用以處理 JSON 數據。
    將 `GetCommentsAsync` 方法的邏輯包裹在 `try-catch` 區塊中，以捕獲異常並輸出錯誤信息。
    新增 `PostCommentsAsync` 和 `ReplyCreationAsync` 方法，分別用於發送主評論和回覆特定評論，並將相關信息添加到視圖中。
    使用 `ImgurAPI.ImgurContext` 與 Imgur API 進行交互，並使用 `CommentsModel.Datum` 構建評論和回覆模型。

--- ImgurApp/ImgurApp/Presenters/CommentsPresenter.cs
+++ ImgurApp/ImgurApp/Presenters/CommentsPresenter.cs
@@ -1,5 +1,7 @@
 ﻿using ImgurAPI.Models;
 using ImgurApp.Contracts;
+using ImgurApp.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +22,60 @@ namespace ImgurApp.Presenters
         public async Task GetCommentsAsync(string galleryId)
         {
             ImgurAPI.ImgurContext context = new ImgurAPI.ImgurContext();
-            CommentsModel response =
+            try
+            {
+                CommentsModel response =
                 await context.Comment.GetComments(galleryId);
-            this._view.ShowComments(response);
+                this._view.ShowComments(response);
+            }
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine($"JSON Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        public async Task PostCommentsAsync(string imageId, string comment, string parentId)
+        {
+            if (imageId != parentId)
+            {
+                await this.ReplyCreationAsync(imageId, parentId, comment);
+                return;
+            }
+            Console.WriteLine("add main");
+
+            parentId = null;
+            ImgurAPI.ImgurContext context = new ImgurAPI.ImgurContext();
+            _ = await context.Comment.CommentCreation(imageId, comment, parentId);
+
+            var commentModel = new CommentsModel.Datum
+            {
+                image_id = imageId,
+                comment = comment,
+                author = AccountModel.Account_url,
+            };
+            this._view.AddCommentToContainer(commentModel);
+        }
+
+        public async Task ReplyCreationAsync(string imageId, string commentId, string comment)
+        {
+            ImgurAPI.ImgurContext context = new ImgurAPI.ImgurContext();
+            var response = await context.Comment.ReplyCreation(imageId, commentId, comment);
+
+            long.TryParse(commentId, out long parentId);
+            var commentModel = new CommentsModel.Datum
+            {
+                image_id = imageId,
+                comment = comment,
+                author = AccountModel.Account_url,
+                parent_id = parentId,
+                id = response.data.id,
+            };
+
+            this._view.AddReplyToSelectedComment(commentModel);
         }
     }
 }
