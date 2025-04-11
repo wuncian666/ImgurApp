60299a96eb183c1f9e593c36719e9b0648f2b54b
wuncian <wuncian666@gmail.com>
2025-04-11 22:41:46 +0800

    新增影像上傳介面與命名空間引用
    新增 `IImageUploadPresenter` 和 `IImageUploadView` 介面，分別提供影像上傳功能和將影像連結新增到文字框的方法。同時引入 `ImgurAPI.Models` 和 `ImgurAPI.Models.Params` 的命名空間引用以支援這些功能。

---
+++ ImgurApp/ImgurApp/Contracts/UploadImageContract.cs
@@ -0,0 +1,20 @@
+﻿using ImgurAPI.Models;
+using ImgurAPI.Models.Params;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurApp.Contracts
+{
+    public interface IImageUploadPresenter
+    {
+        Task ImageUpload(ImageUploadParam uploadParam);
+    }
+
+    public interface IImageUploadView
+    {
+        void AddImageLinkToTextBox(ImageUploadModel image);
+    }
+}
