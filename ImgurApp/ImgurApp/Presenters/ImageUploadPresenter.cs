e31c1cf84b58a45b2a5311c242a227fb304f7f51
wuncian <wuncian666@gmail.com>
2025-04-11 22:40:13 +0800

    新增 ImageUploadPresenter 類別以處理圖片上傳
    實作了 `IImageUploadPresenter` 介面，並新增了 `ImageUpload` 非同步方法。該方法使用 `ImgurAPI` 上傳圖片，並將返回的圖片連結添加到視圖的文本框中。

---
+++ ImgurApp/ImgurApp/Presenters/ImageUploadPresenter.cs
@@ -0,0 +1,28 @@
+﻿using ImgurAPI.Models;
+using ImgurAPI.Models.Params;
+using ImgurApp.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurApp.Presenters
+{
+    public class ImageUploadPresenter : IImageUploadPresenter
+    {
+        private readonly IImageUploadView _view;
+
+        public ImageUploadPresenter(IImageUploadView view)
+        {
+            this._view = view;
+        }
+
+        public async Task ImageUpload(ImageUploadParam uploadParam)
+        {
+            ImgurAPI.ImgurContext context = new ImgurAPI.ImgurContext();
+            ImageUploadModel response = await context.Image.UploadImage(uploadParam);
+            this._view.AddImageLinkToTextBox(response);
+        }
+    }
+}
